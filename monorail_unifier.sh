0b39ab069d104c13c6d5fe997ab55d7367e97b50
pparikh1020 <88831239+pparikh1020@users.noreply.github.com>
2021-12-21 10:34:26 -0500

    Update monorail_unifier.sh
    Updating singularity version and making the reference path more flexible as it has been relocated.

--- monorail_unifier.sh
+++ monorail_unifier.sh
@@ -3,23 +3,25 @@
 # wrapper script to run the monorail unifier step on data 
 # produced from the monorail pump
 # sbatch --mem=128G --cpus-per-task 6 monorail_unifier.sh 
+# /path/to/references
 # /path/to/working/directory /path/to/pump/output
 # /path/to/sample_metadata project_short_name:project_id
 #
 # project id should be between 100 and 250 exclusive
 #
 
-wd=$1
-output=$2
-metadata=$3
-name=$4
+ref=$1
+wd=$2
+output=$3
+metadata=$4
+name=$5
 
-module load singularity/3.8.2
+module load singularity/3.8.5
 
 /bin/bash /home/parikhpp/git/monorail-external/singularity/run_recount_unify.sh \
 	/home/parikhpp/git/monorail-external/recount-unify_1.0.9.sif \
 	hg38 \
-	/data/parikhpp/reference_indexes \
+	$ref \
 	$wd \
 	$output \
 	$metadata \
